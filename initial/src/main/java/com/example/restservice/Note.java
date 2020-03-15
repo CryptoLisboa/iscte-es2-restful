6afd1b17d1e5929f1f3e6f422c1825683203c858
Bcfoo <>
2020-03-15 02:13:44 +0000

    BF -> + Added Note api + Added Rest Controller for Notes + + Added task notes labelled TODO1 .. TODO3 regarding reading and feeding mongo db collections

---
+++ initial/src/main/java/com/example/restservice/Note.java
@@ -0,0 +1,26 @@
+package com.example.restservice;
+
+class Note {
+
+	private final long id;
+	private final String note;
+	
+	public Note(long id, String note) {
+		this.id = id;
+		this.note = note;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getNote() {
+		return note;
+	}
+	
+	@Override
+	public String toString() {
+		return "Note [id=" + id + ", note=" + note + "]";
+	}
+
+}
---
+++ initial/src/main/java/com/example/restservice/NotesController.java
@@ -0,0 +1,34 @@
+package com.example.restservice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotesController {
+	private final AtomicLong counter = new AtomicLong(1);
+	@GetMapping("/note/add")
+	public Note note(@RequestParam(value="note", defaultValue="Lorem Ipson") String note) {
+		// TODO1: instead should get next id from mongo db
+		long id = counter.getAndAdd(1);
+		// END TODO1
+		// TODO2: save newNote to mongo db before returning or on new thread
+		Note newNote = new Note(id, note);
+		// END TODO2
+		return newNote;
+	}
+	@GetMapping("/notes")
+	public List<Note> notes() {
+		List<Note> notes = new ArrayList<Note>();
+		// TODO3: instead should get all existing notes from mongo db
+		notes.add(new Note(1, "Lorem Ipson 1"));
+		notes.add(new Note(2, "Lorem Ipson 2"));
+		notes.add(new Note(3, "Lorem Ipson 3"));
+		// END TODO3
+		return notes;
+	}
+}
