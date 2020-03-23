4960e914f7a3914db3a207d522a9715f50045c1d
Bcfoo <>
2020-03-23 23:10:56 +0000

    BF => + Added change to allow cross origin request

--- src/main/java/com/example/restservice/NotesController.java
+++ src/main/java/com/example/restservice/NotesController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NotesController {
 	private final AtomicLong counter = new AtomicLong(1);
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping("/note/add")
 	public Note note(@RequestParam(value="note", defaultValue="Lorem Ipson") String note) {
 		// TODO1: instead should get next id from mongo db
@@ -21,6 +23,7 @@ public class NotesController {
 		// END TODO2
 		return newNote;
 	}
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping("/notes")
 	public List<Note> notes() {
 		List<Note> notes = new ArrayList<Note>();
