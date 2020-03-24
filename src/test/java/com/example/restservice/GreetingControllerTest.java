46a7f2f899ebbf641598d19d9d16ef615829a30c
Bcfoo <>
2020-03-24 16:34:48 +0000

    Falos-iscteiul => + Added unit tests for the GreetingController and NotesController

---
+++ src/test/java/com/example/restservice/GreetingControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.restservice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GreetingControllerTest {
+
+	@Test
+	public void test() {
+		GreetingController gc = new GreetingController ();
+		assertEquals (gc.greeting("World").getContent(), "Hello, World!");
+	}
+
+}
